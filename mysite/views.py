c009ea17273df1d92bef7c7fe9710c3eeccc49fe
salog0d <david.salomon.nava11@gmail.com>
2025-03-05 20:17:01 -0600

    First commit

---
+++ mysite/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, get_object_or_404
+
+from .models import Person
+
+# Create your views here.
+
+#List of all the people that exist in the database
+def dashboard(request):
+    people = Person.objects.all()
+    return render(request , "dashboard.html" , {"perople" : people})
+
+#Products for each person
+def person_products(request, person_id):
+    person = get_object_or_404(Person, id = person_id)
+    products = person.products.all()
+    return render(request, "person_products.html", {"person": person , "products": products})
